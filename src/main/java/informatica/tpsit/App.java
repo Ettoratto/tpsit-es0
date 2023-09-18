f7ecdfc8bc6a99dfd538895afcb4b1545e4bec90
lai2-01 <lai2-01@itismeucci.com>
2023-09-18 11:36:16 +0200

    first commit

---
+++ src/main/java/informatica/tpsit/App.java
@@ -0,0 +1,13 @@
+package informatica.tpsit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
